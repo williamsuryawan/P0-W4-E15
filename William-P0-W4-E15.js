ff3d3f70db6603d53e5b7b0aaaec1f10b9a38e86
William Suryawan <suryawan.william@gmail.com>
2019-01-21 00:03:56 +0700

    tambah file

---
+++ William-P0-W4-E15.js
@@ -0,0 +1,72 @@
+function highestScore(arr) {
+  const highestByClass = arr.reduce((a, student) => {
+    const { class: className, score } = student;
+    if (!a[className] || a[className].score < score) {
+      a[className] = student;
+    }
+    return a;
+  }, {});
+  return Object.values(highestByClass);
+}
+
+
+// TEST CASE
+console.log(highestScore([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+console.log(highestScore([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: { name: 'Alexander', score: 100 },
+//   wolves: { name: 'Alisa', score: 76 },
+//   tigers: { name: 'Viktor', score: 80 }
+// }
+
+
+console.log(highestScore([])); //{}
